98ed8463d3f13c65b42b63d4253ff5689582aa4b
Florent Ulvé <florent.ulve@protonmail.com>
2024-08-20 07:16:23 +0200

    first step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::all, clippy::pedantic)]
+use std::error::Error;
+
+mod webapp;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    
+    webapp::boostrap().await;
+
+    Ok(())
 }
